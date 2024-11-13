67b4aa09089914648b6ba45ad4008f50eec144a7
Hinf1nity <dacalvimontes@gmail.com>
2024-11-12 20:01:45 -0400

    Git corrections and add bash code

--- examen2_ws/build/ejercicio1/CMakeFiles/ejercicio1_uninstall.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty custom commands generated dependencies file for ejercicio1_uninstall.
-# This may be replaced when dependencies are built.
--- examen2_ws/build/ejercicio4/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/integralmilk03/robotica_2_2024/examen2_ws/install/ejercicio4'
